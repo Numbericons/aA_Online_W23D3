4bcfecc5704fc42f33e042bc48237f8d238cefe3
Zachary <zachjoliver@gmail.com>
2019-09-08 10:53:46 -0700

    Finish BFS

--- depth_breadth_first_project/lib/breadth_first_array.js
+++ depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,5 +1,13 @@
-function breadthFirstArray(root) {
-
+function breadthFirstArray(root) { //
+    let retArr = [];
+    let queue = [root]; // [a]
+    while (queue.length) { // [f]
+        let node = queue.shift(); //f
+        retArr.push(node.val); //[a, b, c, d, e, f]
+        if (node.left) queue.push(node.left); //[]
+        if (node.right) queue.push(node.right); // []
+    }
+    return retArr; // [a, b, c, d, e, f]
 }
 
 module.exports = {

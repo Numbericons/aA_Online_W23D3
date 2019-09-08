99f00c7a824fe9eaf3a708795e860cfd0749c3eb
Zachary <zachjoliver@gmail.com>
2019-09-08 11:19:20 -0700

    Add tree height recursive solution

--- depth_breadth_first_project/lib/tree_height.js
+++ depth_breadth_first_project/lib/tree_height.js
@@ -1,14 +1,34 @@
+// function treeHeight(root) {
+//     if (!root) return -1;
+//     let maxEdges = 0;
+//     let queue = [root];
+//     while (queue.length) {
+//         let node = queue.shift();
+//         if (node.left || node.right) maxEdges += 1;
+//         if (node.left) queue.push(node.left);
+//         if (node.right) queue.push(node.right);
+//     }
+//     return maxEdges;
+// }
+
 function treeHeight(root) {
     if (!root) return -1;
-    let maxEdges = 0;
-    let queue = [root];
-    while (queue.length) {
-        let node = queue.shift();
-        if (node.left || node.right) maxEdges += 1;
-        if (node.left) queue.push(node.left);
-        if (node.right) queue.push(node.right);
-    }
-    return maxEdges;
+    return 1 + Math.max(treeHeight(root.left), treeHeight(root.right));
+    // let left, right;
+    // let edges = 0;
+    // if (root.left) {
+    //     edges += 1;
+    //     left = treeHeight(root.left); 
+    // } else {
+    //     left = 0;
+    // }
+    // if (root.right) {
+    //     edges += 1;
+    //     right = treeHeight(root.right);
+    // } else {
+    //     right = 0;
+    // }
+    // return (left >= right) ? left + edges: right + edges;
 }
 
 

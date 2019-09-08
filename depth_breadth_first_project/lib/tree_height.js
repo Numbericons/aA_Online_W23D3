3cfb9bb0b078d737e6b603063ace4f5aa72adeec
Zachary <zachjoliver@gmail.com>
2019-09-08 10:59:41 -0700

    Finish depth Breath First Project and W23D3

--- depth_breadth_first_project/lib/tree_height.js
+++ depth_breadth_first_project/lib/tree_height.js
@@ -1,5 +1,14 @@
 function treeHeight(root) {
     if (!root) return -1;
+    let maxEdges = 0;
+    let queue = [root];
+    while (queue.length) {
+        let node = queue.shift();
+        if (node.left || node.right) maxEdges += 1;
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    return maxEdges;
 }
 
 
